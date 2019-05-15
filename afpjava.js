b6b35abfcafa5a46ee1904002305e6e002b03576
CameronFlatt <CameronFlatt@users.noreply.github.com>
2019-05-15 10:53:09 -0500

    not sure
    yeah whatever

--- afpjava.js
+++ afpjava.js
@@ -12,15 +12,3 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
-
-$(document).ready(function() {
-  $('.menu-trigger').click(function() {
-    $('nav ul').slideToggle(500);
-});
-
-  $(window).resize(function() {
-	   if (  $(window).width() > 700 ) {
-		$('nav ul').removeAttr('style');
-	 }
-	});
-});
